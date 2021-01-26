0e99647561a4349b9ddf3abd837cfed1201039f7
Samad Hatsijev <crysis90war@hotmail.be>
2021-01-26 14:50:10 +0100

    Update settings.py
    import Django_heroku
    # Configure Django App for Heroku.
    django_heroku.settings(locals())

--- lemka_api/settings.py
+++ lemka_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 from datetime import timedelta
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -200,3 +201,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
