6d25f1cdea54dac61a7ef7150a62c6714c0ba4d2
Samad Hatsijev <crysis90war@hotmail.be>
2021-01-27 12:00:17 +0100

    Implémentation AWS S3
    Correction de static_root

--- lemka_api/settings.py
+++ lemka_api/settings.py
@@ -154,11 +154,8 @@ if USE_S3 and USE_S3 == 'True':
 
     AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATIC_URL = '/static/'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
 
     MEDIA_ROOT = 'media'
     MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIA_ROOT)
