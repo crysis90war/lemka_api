f80da2f5f3136bbcc987fcd1074df1b0c2c6c2ec
Samad Hatsijev <crysis90war@hotmail.be>
2021-01-27 18:01:13 +0100

    Suppression
    code dans models supprimé pour resize.
    On dirait que meme s'il est commenté, il l'éxecute quand meme.

--- lemka/models.py
+++ lemka/models.py
@@ -68,8 +68,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
         self.username = self.username.lower()
         self.email = self.email.lower()
-        # if self.image:
-        #     Utils.resize_image(self.image.path, (360, 360))
 
     def tokens(self):
         refresh = RefreshToken.for_user(self)
