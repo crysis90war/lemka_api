b54acc0c36f5e6948be92910ca3cb1b1abad1e8e
Samad Hatsijev <crysis90war@hotmail.be>
2021-03-17 17:38:50 +0100

    UserDemandeSerializer
    extra_kwargs added to numero_demande_devis for read_only True

--- utilisateur/serializers.py
+++ utilisateur/serializers.py
@@ -4,7 +4,6 @@ from lemka.models import DemandeDevis
 
 
 class UserDemandeDevisSerializer(serializers.ModelSerializer):
-    numero_demande_devis = serializers.IntegerField(read_only=True)
     est_traite = serializers.BooleanField(default=False, read_only=True)
     est_urgent = serializers.BooleanField(default=False)
     est_soumis = serializers.BooleanField(default=False)
