e55d6b0888602fb802855e307351c633e9abb74f
Samad Hatsijev <crysis90war@hotmail.be>
2021-03-21 10:19:10 +0100

    permissions.py

--- lemka/permissions.py
+++ lemka/permissions.py
@@ -76,4 +76,4 @@ class IsOwnerOrAdmin(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         # if request.method in permissions.SAFE_METHODS:
         #     return True
-        return obj.owner == request.user or request.user.is_admin
+        return obj.ref_user_mensuration.ref_user == request.user or request.user.is_admin
