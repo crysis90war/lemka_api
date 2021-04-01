14446d4de5bf2dcc968bce27ea92048413f5b468
Samad Hatsijev <crysis90war@hotmail.be>
2021-04-02 00:06:40 +0200

    added field in devis
    updated_at

--- lemka/models.py
+++ lemka/models.py
@@ -363,6 +363,7 @@ class DemadeDevisImage(models.Model):
 class Devis(models.Model):
     numero_devis = models.PositiveIntegerField()
     created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
     remarque = models.TextField()
     est_accepte = models.BooleanField(null=True, blank=True)
     est_soumis = models.BooleanField(default=False)
