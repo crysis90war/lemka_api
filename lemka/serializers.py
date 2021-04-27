9adfa364e1f036828eb4ae0867e8edf7c069e665
Samad Hatsijev <crysis90war@hotmail.be>
2021-04-27 12:41:17 +0200

    signals.py mercerie image post delete

--- lemka/serializers.py
+++ lemka/serializers.py
@@ -12,7 +12,6 @@ class GlobalMercerieSerializer(serializers.ModelSerializer):
     caracteristiques = serializers.SerializerMethodField(read_only=True)
     reference = serializers.CharField(read_only=True)
     images = serializers.SerializerMethodField(read_only=True)
-    name = serializers.SerializerMethodField(read_only=True)
     tva = serializers.SerializerMethodField(read_only=True)
     couleur = serializers.SerializerMethodField(read_only=True)
 
@@ -23,12 +22,6 @@ class GlobalMercerieSerializer(serializers.ModelSerializer):
             'ref_tva': {'write_only': True},
             'ref_couleur': {'write_only': True},
         }
-
-    # noinspection PyMethodMayBeStatic
-    def get_name(self, instance):
-        name = instance.ref_mercerie.nom
-        couleur = instance.ref_couleur.nom
-        return f'{name} - {couleur}'
 
     # noinspection PyMethodMayBeStatic
     def get_caracteristiques(self, instance):
