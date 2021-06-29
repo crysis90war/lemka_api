0f434b1490706cd62def98646e0ab75eae34d528
Samad Hatsijev <crysis90war@hotmail.Be>
2021-06-29 22:03:19 +0200

    unused files have been removed.

--- lemka_api/urls.py
+++ lemka_api/urls.py
@@ -6,11 +6,6 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework.documentation import include_docs_urls
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from . import views
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -35,7 +30,6 @@ urlpatterns = [
     path('api/profil/', include('utilisateur.urls')),
     path('api/auth/', include('authentication.urls')),
     path('api/auth-social/', include(('social_auth.urls', 'social_auth'), namespace="social_auth"))
-    # path('api/', views.api_root),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
